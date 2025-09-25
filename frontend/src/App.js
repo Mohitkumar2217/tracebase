bc08f21201966becee38ef29bc85109f2e9ebb1c
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-09-25 14:28:12 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import ReadPopup from "./ReadPopup";
+// import ReadPopup from "./ReadPopup";
 import { injectAnimationStyles } from "./AnimationStyles";
 
 // --- Green-White Color Palette ---
